f2a2b008b44808025f6a7885107476a568c68918
jossvagui <joss.vagui@gmail.com>
2022-06-30 12:00:48 -0500

    header listo

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Header from '../components/header/Header'
+
+const Inicio = () => {
+  return (
+    <Fragment>        
+        <Header/>
+    </Fragment>
+  )
+}
+
+export default Inicio
