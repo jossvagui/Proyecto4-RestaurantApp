d5c7052e88a781c886760197a32b477f51b1b6c9
jossvagui <joss.vagui@gmail.com>
2022-07-02 11:08:52 -0500

    hero

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react'
-import Header from '../components/header/Header'
+import Header from '../components/header/Header';
+import HeroSlider from '../components/hero-slider/HeroSlider';
+import PopularMenu from '../components/popular-menu/PopularMenu';
+
 
 const Inicio = () => {
   return (
     <Fragment>        
         <Header/>
+        <HeroSlider/>
+        <PopularMenu/>
     </Fragment>
   )
 }
